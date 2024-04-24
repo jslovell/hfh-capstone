c510823f9959118935869e556a7e915e1a316153
Osvaldo Guzman <75756076+Oz-Guzman@users.noreply.github.com>
2024-04-23 20:50:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="jquery-ui.css"> 
+    <link rel="stylesheet" href="styles/indexStyle.css"> 
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="jquery-ui.css"></script>
+    <script src="script.js"></script>
+
+    <title>CS491</title>
+    
+</head>
+<?php include "navbar.phtml" ?>
+<body>
+
+    <h1 id="page-title" class="page-title">Habitat for Humanity</h1>
+
+</body>
+
+</html>
--- script.js
+++ script.js
@@ -1,170 +1,300 @@
-const form = document.getElementById('assessment-form');
-const output = document.getElementById('output');
+$(document).ready(function() {
+	var activeButtonId = null;
+	var isBrushActive = false;
+	var isSelectMode = false;
+	var iconCounter = localStorage.getItem('iconCounter') ? parseInt(localStorage.getItem('iconCounter')) : 0;
+	var iconId = null;
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+	function saveIconData(iconId, type, data) {
+		var iconData = {
+			id: iconId,
+			type: type,
+			data: data,
+		};
+		localStorage.setItem(iconId, JSON.stringify(iconData));
+	}
 
-    const photoInput = document.getElementById('photo-upload');
-    const annotationInput = document.getElementById('annotation');
+	function loadIconData(iconId) {
+		let iconData = JSON.parse(localStorage.getItem('iconData')) || {};
+		return iconData[iconId];
+	}
 
-    // This is just to simulate a server response
-    const assessmentData = {
-        photo: photoInput.files[0].name,
-        annotation: annotationInput.value
-    };
 
-    const response = `
-        <p>Assessment submitted successfully:</p>
-        <p><strong>Photo:</strong> ${assessmentData.photo}</p>
-        <p><strong>Annotation:</strong> ${assessmentData.annotation}</p>
-    `;
+	$('body').on("click", '.alerts-icon', function(e) {
+		e.preventDefault();
+		var popupContent = "<select><option value='1'>Electrical</option><option value='2'>Windows</option><option value='3'>Water Damage</option><option value='4'>Asbestos</option><option value='5'>Cracks</option><option value='other'>Other</option></select><br><textarea id='alerts-textarea' placeholder='Enter description'></textarea><br><input type='file' accept='image/*'><br><button class='save-button'>Save</button>";
+		var $popup = $("<div class='alerts'></div>");
+		$popup.html(popupContent);
+		$popup.dialog({
+			width: 600,
+			height: 'auto',
+			modal:true,
+			title: 'Input Alert',
+			overlay: { backgroundColor: "#000", opacity: 0.9 }
+		});
+		$('#save-button').on('click', function() {
+			var textareaValue = $('#alerts-textarea').val();
+			saveIconData(iconId, 'alerts', { textareaValue: textareaValue });
+		});
 
-    output.innerHTML = response;
+		var iconData = loadIconData(iconId);
+		if (iconData && iconData.type === 'alerts') {
+			$('#alerts-textarea').val(iconData.data.textareaValue);
+		}
+	});
 
-    const sameAddressCheckbox = document.getElementById('same-address');
-    const affectedAddressContainer = document.getElementById('affected-address-container');
+	$('body').on("click", '.notes-icon', function(e) {
+		e.preventDefault();
+		var popupContent = "<textarea id='notes-textarea' placeholder='Enter description'></textarea><button class='save-button'>Save</button>";
+		var $popup = $("<div class='alerts'></div>");
+		$popup.html(popupContent);
+		$popup.dialog({
+			width: 600,
+			height: 'auto',
+			modal:true,
+			title: 'Input Notes',
+			overlay: { backgroundColor: "#000", opacity: 0.9 }
+		});
+		$('#save-button').on('click', function() {
+			var textareaValue = $('#notes-textarea').val();
+			saveIconData(iconId, 'notes', { textareaValue: textareaValue });
+		});
+	});
 
-    sameAddressCheckbox.addEventListener('change', function() {
-        if (this.checked) {
-            affectedAddressContainer.style.display = 'none';
-        } else {
-            affectedAddressContainer.style.display = 'block';
-        }
-    });
+	$('body').on("click", '.photo-icon', function(e) {
+		e.preventDefault();
+		var popupContent = "<textarea id='photo-textarea' placeholder='Enter description'></textarea><br><input type='file' accept='image/*'><br><button class='save-button'>Save</button>";
+		var $popup = $("<div class='alerts'></div>");
+		$popup.html(popupContent);
+		$popup.dialog({
+			width: 600,
+			height: 'auto',
+			modal:true,
+			title: 'Input Picture',
+			overlay: { backgroundColor: "#000", opacity: 0.9 }
+		});
+		$('#save-button').on('click', function() {
+			var textareaValue = $('#photo-textarea').val();
+			saveIconData(iconId, 'photo', { textareaValue: textareaValue });
+		});
+	});
+
+	$('body').on('click', '.save-button', function() {
+		var textareaValue;
+		if ($(this).closest('.alerts').length) {
+			textareaValue = $('#alerts-textarea').val();
+			saveIconData(iconId, 'alerts', { textareaValue: textareaValue });
+		} else if ($(this).closest('.notes').length) {
+			textareaValue = $('#notes-textarea').val();
+			saveIconData(iconId, 'notes', { textareaValue: textareaValue });
+		} else if ($(this).closest('.photo').length) {
+			textareaValue = $('#photo-textarea').val();
+			saveIconData(iconId, 'photo', { textareaValue: textareaValue });
+		}
+		$(this).closest('.alerts').dialog('close');
+	});
+
+
+	$(function(){
+
+		$('.photo-icon').on("click", function(e){
+			e.preventDefault();
+			var popupContent = "<textarea placeholder='Enter description'></textarea><br><input type='file' accept='image/*'><br><button onclick='alert('upload complete')'>Upload</button>";
+			var $popup = $("<div class='alerts'></div>");
+			$popup.html(popupContent);
+			$popup.dialog({
+				width: 600,
+				height: 'auto',
+				modal:true,
+				title: 'Input Picture',
+				overlay: { backgroundColor: "#000", opacity: 0.9 }
+			});
+		});
+
+		$('.alerts-icon').on("click", function(e){
+			e.preventDefault();
+			var popupContent = "<select><option value='1'>Electrical</option><option value='2'>Windows</option><option value='3'>Water Damage</option><option value='4'>Asbestos</option><option value='5'>Cracks</option><option value='other'>Other</option></select><br><textarea placeholder='Enter description'></textarea><br><input type='file' accept='image/*'><br><button>Submit</button>";
+			var $popup = $("<div class='alerts'></div>");
+			$popup.html(popupContent);
+			$popup.dialog({
+				width: 600,
+				height: 'auto',
+				modal:true,
+				title: 'Input Alert',
+				overlay: { backgroundColor: "#000", opacity: 0.9 }
+			});
+		});
+
+		$('body').on("click", '.notes-icon', function(e) {
+			e.preventDefault();
+			var popupContent = "<textarea placeholder='Enter description'></textarea><button>Submit</button>";
+			$popup.html(popupContent);
+			$popup.dialog({
+				width: 600,
+				height: 'auto',
+				modal:true,
+				title: 'Input Notes',
+				overlay: { backgroundColor: "#000", opacity: 0.9 }
+			});
+		});
+
+	});
+
+
+	function toggleSideNav() {
+		$(".side_nav,.nav-overlay").toggleClass("active");
+	}
+
+	$(".nav_ico").click(function() {
+		$(this).toggleClass("active");
+		toggleSideNav();
+		return false;
+	});
+
+	$(".nav-overlay").click(function() {
+		$(".nav_ico").removeClass("active");
+		toggleSideNav();
+	});
+
+	$(".side_nav a[data-target]").click(function(event) {
+		event.preventDefault();
+		const targetId = $(this).attr('data-target');
+		const targetNav = $("#" + targetId);
+
+		if (targetNav) {
+			$(".nav_ico").removeClass("active");
+			toggleSideNav();
+		}
+	});
+
+
+	function toggleBrush() {
+		isBrushActive = !isBrushActive;
+		$("body").toggleClass("brush-cursor", isBrushActive);
+	}
+
+	function deactivateIcons() {
+		$("#alert-button").css("background-image", "url('images/alert-button.png')");
+		$("#alert-moderate-button").css("background-image", "url('images/alert-moderate-button.png')");
+		$("#alert-severe-button").css("background-image", "url('images/alert-severe-button.png')");
+		$("#photo-button").css("background-image", "url('images/picture-button.png')");
+		$("#note-button").css("background-image", "url('images/note-button.png')");
+	}
+
+	function toggleSelectMode() {
+		isSelectMode = !isSelectMode;
+		if (isSelectMode) {
+			$("#select-button").css("background-image", "url('images/select-button-active.png')");
+			toggleBrush();
+		} else {
+			$("#select-button").css("background-image", "url('images/select-button.png')");
+			deactivateIcons()
+			isBrushActive = false;
+		}
+	}
+
+	$("#select-button").click(function() {
+		toggleSelectMode();
+	});
+
+	$(".icon-button").on("click", function() {
+		if (isSelectMode) {
+			activeButtonId = $(this).attr("id");
+		}
+	});
+
+	$("#alert-severe-button, #alert-moderate-button, #alert-button, #photo-button, #note-button, #removal-button").on("click", function() {
+		if (isSelectMode) {
+			activeButtonId = $(this).attr("id");
+		}
+	});
+
+	$(".clickableArea").on("click", function(event) {
+
+
+		if (isSelectMode && activeButtonId) {
+			var bodyOffset = $("body").offset();
+			var iconTop = event.pageY - bodyOffset.top - 10;
+			var iconLeft = event.pageX - bodyOffset.left - 10;
+			var $icon;
+			switch(activeButtonId) {
+				case "alert-severe-button":
+					deactivateIcons()
+					$("#alert-severe-button").css("background-image", "url('images/alert-severe-button-active.png')");
+					$icon = $("<div class=\"box-alert\"><div class=\"alerts-icon\"><img src=\"images/alert-sever-icon.png\"></div></div>");
+					$icon.attr("id", "icon-" + iconCounter);
+					iconCounter++;
+					$icon.css({
+						top: iconTop,
+						left: iconLeft
+					});
+					break;
+				case "alert-moderate-button":
+					deactivateIcons()
+					$("#alert-moderate-button").css("background-image", "url('images/alert-moderate-button-active.png')");
+					$icon = $("<div class=\"box-alert\"><div class=\"alerts-icon\"><img src=\"images/alert-moderate-icon.png\"></div></div>");
+					$icon.attr("id", "icon-" + iconCounter);
+					iconCounter++;
+					$icon.css({
+						top: iconTop,
+						left: iconLeft
+					});
+					break;
+				case "alert-button":
+					deactivateIcons()
+					$("#alert-button").css("background-image", "url('images/alert-button-active.png')");
+					$icon = $("<div class=\"box-alert\"><div class=\"alerts-icon\"><img src=\"images/alert-icon.png\"></div></div>");
+					$icon.attr("id", "icon-" + iconCounter);
+					iconCounter++;
+					$icon.css({
+						top: iconTop,
+						left: iconLeft
+					});
+					break;
+				case "photo-button":
+					deactivateIcons()
+					$("#photo-button").css("background-image", "url('images/picture-button-active.png')");
+					$icon = $("<div class=\"box\"><div class=\"photo-icon\"><img src=\"images/picture-icon.png\"></div><div class=\"picture\">Photo Here...<div></div>");
+					$icon.attr("id", "icon-" + iconCounter);
+					iconCounter++;
+					$icon.css({
+						top: iconTop,
+						left: iconLeft
+					});
+
+					break;
+				case "note-button":
+					deactivateIcons()
+					$("#note-button").css("background-image", "url('images/note-button-active.png')");
+					$icon = $("<div class=\"box-note\"><div class=\"notes-icon\"><img src=\"images/note-icon.png\"></div><div class=\"notes\"></div></div>");
+					$icon.attr("id", "icon-" + iconCounter);
+					iconCounter++;
+					$icon.css({
+						top: iconTop,
+						left: iconLeft
+					});
+					break;
+			}
+
+			$("body").append($icon);
+			saveIconData($icon.attr("id"), 'icon', { top: iconTop, left: iconLeft });
+			localStorage.setItem('iconCounter', iconCounter.toString());
+		}
+
+		iconId = $icon.attr("id");
+	});
+
+
+	$("#clearButton").on("click", function() {
+		let text = "Warning: All icons will be deleted\n";
+		if (confirm(text)) {
+			$(".box, .box-alert, .box-note").remove();
+			deactivateIcons();
+			isSelectMode = false;
+			isBrushActive = false;
+			iconCounter = 0;
+			$("#select-button").css("background-image", "url('images/select-button.png')");
+			localStorage.clear();
+		}
+	});
 });
-
-!(function(d){
-    // Variables to target our base class, get carousel items, count how many carousel items there are, set the slide to 0 (which is the number that tells us the frame we're on), and set motion to true which disables interactivity.
-    var itemClassName = "carousel__photo";
-    items = d.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0,
-        moving = true;
-
-    // To initialize the carousel we'll want to update the DOM with our own classes
-    function setInitialClasses() {
-
-        // Target the last, initial, and next items and give them the relevant class.
-        // This assumes there are three or more items.
-        items[totalItems - 1].classList.add("prev");
-        items[0].classList.add("active");
-        items[1].classList.add("next");
-    }
-
-    // Set click events to navigation buttons
-
-    function setEventListeners() {
-        var next = d.getElementsByClassName('carousel__button--next')[0],
-            prev = d.getElementsByClassName('carousel__button--prev')[0];
-
-        next.addEventListener('click', moveNext);
-        prev.addEventListener('click', movePrev);
-    }
-
-    // Disable interaction by setting 'moving' to true for the same duration as our transition (0.5s = 500ms)
-    function disableInteraction() {
-        moving = true;
-
-        setTimeout(function(){
-            moving = false
-        }, 500);
-    }
-
-    function moveCarouselTo(slide) {
-
-        // Check if carousel is moving; if not, allow interaction
-        if(!moving) {
-
-            // Temporarily disable interactivity
-            disableInteraction();
-
-            // Preemptively set variables for the current next and previous slide, as well as the potential next or previous slide.
-            var newPrevious = slide - 1,
-                newNext = slide + 1,
-                oldPrevious = slide - 2,
-                oldNext = slide + 2;
-
-            // Test if carousel has more than three items
-            if ((totalItems - 1) > 3) {
-
-                // Checks if the new potential slide is out of bounds and sets slide numbers
-                if (newPrevious <= 0) {
-                    oldPrevious = (totalItems - 1);
-                } else if (newNext >= (totalItems - 1)){
-                    oldNext = 0;
-                }
-
-                // Check if the current slide is at the beginning or end and sets slide numbers
-                if (slide === 0) {
-                    newPrevious = (totalItems - 1);
-                    oldPrevious = (totalItems - 2);
-                    oldNext = (slide + 1);
-                } else if (slide === (totalItems - 1)) {
-                    newPrevious = (slide - 1);
-                    newNext = 0;
-                    oldNext = 1;
-                }
-
-                // Now we've worked out where we are and where we're going, by adding and removing classes, we'll be triggering the carousel's transitions.
-
-                // Based on the current slide, reset to default classes.
-                items[oldPrevious].className = itemClassName;
-                items[oldNext].className = itemClassName;
-
-                // Add the new classes
-                items[newPrevious].className = itemClassName + " prev";
-                items[slide].className = itemClassName + " active";
-                items[newNext].className = itemClassName + " next";
-            }
-        }
-    }
-
-    // Next navigation handler
-    function moveNext() {
-
-        // Check if moving
-        if (!moving) {
-
-            // If it's the last slide, reset to 0; otherwise, +1
-            if (slide === (totalItems - 1)) {
-                slide = 0;
-            } else {
-                slide++;
-            }
-
-            // Move carousel to the updated slide
-            moveCarouselTo(slide);
-        }
-    }
-
-    // Previous navigation handler
-    function movePrev() {
-
-        // Check if moving
-        if (!moving) {
-
-            // If it's the first slide, set as the last slide; otherwise, -1
-            if (slide === 0) {
-                slide = (totalItems - 1);
-            } else {
-                slide--;
-            }
-
-            // Move carousel to the updated slide
-            moveCarouselTo(slide);
-        }
-    }
-
-    // Initialize carousel
-    function initCarousel() {
-        setInitialClasses();
-        setEventListeners();
-
-        // Set moving to false now that the carousel is ready
-        moving = false;
-    }
-
-    // Call the initialization function to set up the carousel
-    initCarousel();
-
-}(document));
