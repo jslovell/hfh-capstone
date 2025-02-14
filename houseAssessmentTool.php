5913c4693822823f4b87c9a46026dfaa86a72238
“IsaacSishere” <isullivan@mail.bradley.edu>
2025-02-14 16:08:36 -0600

    House Assessment Forum Redesign Phase 1
    Phase one of the house redesign. Making the design more in line with the future of how we want the website to look. Phase one because I want to split the jquery doc up to make it more readable and workable for front end

--- houseAssessmentTool.php
+++ houseAssessmentTool.php
@@ -1,15 +1,16 @@
 <!-- PHP used to ensure that user is logged in -->
 <?php include "./php_scripts/session.php" ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles/toolStyle.css">
+    <!-- link rel="stylesheet" href="styles/toolStyle.css" -->
     <link rel="stylesheet" href="jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="jquery-ui.css"></script>
+    <!-- <script src="jquery-ui.css"></script> -->
+<!-- -->
+    <link rel="stylesheet" href="tempHAStyle.css">
     <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <title>House Assessment Tool</title>
     <?php include "navbar.php" ?>
 
@@ -44,45 +45,84 @@
     </script>
 
 -->
-
 </head>
-<body style="margin-top: 8%">
-<h1>House Assessment Tool</h1>
-<form id="assessment-form" action="./php_scripts/form.php" method="post" enctype="multipart/form-data">
-<!--
-    <label for="photoupload">Upload Photo (PNG or JPG):</label>
-    <input type="file" name="photoupload" id="photoupload" accept="image/jpeg,image/png" required>
-    <textarea name="annotation" id="annotation" placeholder="Add comments"></textarea>
--->
+    <body class="background">
+    <div class="card">
+        <container class="container">
+            <h1 class="text-center header">House Assessment Tool</h1>
+            <form id="assessment-form" action="./php_scripts/form.php" method="post" enctype="multipart/form-data">
+            <!--
+                <label for="photoupload">Upload Photo (PNG or JPG):</label>
+                <input type="file" name="photoupload" id="photoupload" accept="image/jpeg,image/png" required>
+                <textarea name="annotation" id="annotation" placeholder="Add comments"></textarea>
+            -->
 
-    <h2>Client Information</h2>
-
-    <!-- Sections for contact info -->
-    <label for="firstname">First Name:</label>
-    <input type="text" name="firstname" id="firstname" maxlength="32" required>
-    <label for="lastname">Last Name:</label>
-    <input type="text" name="lastname" id="lastname" maxlength="32" required>
-    <label for="email">Email Address (Optional):</label>
-    <input type="email" name="email" id="email" maxlength="64" >
-    <label for="phone">Phone Number:</label>
-    <input type="tel" name="phone" id="phone" maxlength="16" required>
-    <label for="address">Address:</label>
-    <input type="text" name="address" id="address" maxlength="64" required>
-    <label for="city">City:</label>
-    <input type="text" name="city" id="city" maxlength="32" required>
-    <label for="state">State:</label>
-    <input type="text" name="state" id="state" maxlength="32" required>
-
-    <label for="zip">Zip Code:</label>
-    <input type="text" name="zip" id="zip" maxlength="8" required>
-
-    <label for="layout">Home Layout:</label>
-    <input type="file" name="layout" id="layout">
-
-    <br>
-    <button type="submit" name="submit">Submit Assessment</button>
-
-</form>
-<div id="output"></div>
+            <!-- Sections for contact info -->
+             <div class ="inputparam text-center">
+                <label for="firstname">First Name:</label>
+                <div class ="box">
+                <input type="text" name="firstname" id="firstname" maxlength="32" required placeholder="Joe">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="lastname">Last Name:</label>
+                <div class ="box">
+                <input type="text" name="lastname" id="lastname" maxlength="32" required placeholder="Smith">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="email">Email Address (Optional):</label>
+                <div class ="box">
+                    <!-- previous type was email -->
+                <input type="text" name="email" id="email" maxlength="64" placeholder="example@gmail.com">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="phone">Phone Number:</label>
+                <div class ="box">
+                    <!-- previous type was tel -->
+                <input type="text" name="phone" id="phone" maxlength="16" required placeholder="555-555-5555">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="address">Address:</label>
+                <div class ="box">
+                <input type="text" name="address" id="address" maxlength="64" required placeholder="832 N State Drive">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="city">City:</label>
+                <div class ="box">
+                <input type="text" name="city" id="city" maxlength="32" required placeholder="St. Louis, Peoria, etc.">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="state">State:</label>
+                <div class ="box">
+                <input type="text" name="state" id="state" maxlength="32" required placeholder="Missouri, Illinois, etc.">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="zip">Zip Code:</label>
+                <div class ="box">
+                <input type="text" name="zip" id="zip" maxlength="8" required placeholder="55555">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <label for="layout">Home Layout:</label>
+                <div class ="box">
+                <input type="file" name="layout" id="layout">
+                </div>
+            </div>
+            <div class ="inputparam text-center">
+                <div class ="box">
+                <button type="submit" name="submit">Submit Assessment</button>
+                </div>
+            </div>
+            </form>
+            <div id="output"></div>
+        </container>
+    </div>
 </body>
+
 </html>
