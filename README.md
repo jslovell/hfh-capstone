c817d4ef9a87a5013975ca7e6707655c88105f46
Josh <jlovell@mail.bradley.edu>
2025-04-03 11:25:39 -0500

    Update README with developer installation steps

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # CS-490-Capstone
 
-Home assessment tool for the Habitat for Humanity Aging in Place Program.
+Home assessment tool for the Habitat for Humanity Aging in Place Program, Des Moines IA.
+This is used to annotate home layouts with information that may be needed by contractors.
 
-## Installation
+## Production Installation
 
 In order to use this project, you will need both an Apache PHP server and a MySQL database.
-The contents of this repository can be copied into the `htdocs` directory within your Apache server. The tables for the database can be generated using [insert file].
+Then the following steps can be taken:
+1. Copy the contents of this repository into the `htdocs` directory within your Apache server.
+2. Import the necessary tables into database using `hfh.sql`.
+3. Configure the database connection in `php_scripts\db.php`, particularly the username and password.
+
+All other dependencies are included in the repository.
+
+## Developer Installation
+
+Developers will need to have [XAMPP Control Panel](https://www.apachefriends.org) installed on their computers.
+Then the following steps can be taken:
+1. Clone this repository either directly into the `htdocs` directory or into some directory `htdocs\<name>`. This is usually found in `C:\xampp\htdocs`.
+3. Start the XAMPP Apache and MySQL modules.
+2. Create a new database and import the necessary tables using `hfh.sql`.
+3. Configure the database connection in `php_scripts\db.php`, particularly the username, password, and dbname. The "Localhost Database Variables" often work by default.
+4. Connect to the web app using either `http://localhost/` or `http://localhost/<name>/`, depending on your choices in step 1.
 
 ## Use
 
---
+++ hfh.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2025 at 10:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hfh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_entries`
+--
+
+CREATE TABLE `form_entries` (
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `parcel_number` varchar(45) DEFAULT 'N/A',
+  `zip` varchar(45) DEFAULT NULL,
+  `layout` varchar(45) DEFAULT NULL,
+  `assessmentStatus` enum('Needs Assessment','Needs Bidding') NOT NULL DEFAULT 'Needs Assessment',
+  `username` varchar(45) DEFAULT 'unknown user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icons`
+--
+
+CREATE TABLE `icons` (
+  `id` int(11) NOT NULL,
+  `iconId` varchar(255) DEFAULT NULL,
+  `assignmentID` int(11) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `x_pos` int(11) NOT NULL,
+  `y_pos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcel`
+--
+
+CREATE TABLE `parcel` (
+  `parcel_number` varchar(20) NOT NULL,
+  `address` varchar(90) DEFAULT NULL,
+  `residence_type` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `first_name` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`);
+
+--
+-- Indexes for table `form_entries`
+--
+ALTER TABLE `form_entries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `icons`
+--
+ALTER TABLE `icons`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `iconId` (`iconId`);
+
+--
+-- Indexes for table `parcel`
+--
+ALTER TABLE `parcel`
+  ADD PRIMARY KEY (`parcel_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `form_entries`
+--
+ALTER TABLE `form_entries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `icons`
+--
+ALTER TABLE `icons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
