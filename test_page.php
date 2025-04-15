fb696e90359326dca9edb43876373153321f7506
bbeardsley10 <brenton.beardsley@gmail.com>
2025-04-15 12:46:12 -0500

    Renamed the image since the logic when pressing the button needs all of the files names to be lowercase so file path matches up from script.js

--- test_page.php
+++ test_page.php
@@ -96,7 +96,7 @@ mysqli_close($conn);
             background-image: url("images/plumbing.png");
         }
         #hvac-button {
-            background-image: url("images/HVAC.png");
+            background-image: url("images/hvac.png");
         }
         #door-button {
             background-image: url("images/door.png");
